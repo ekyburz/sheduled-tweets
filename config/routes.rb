bae6d01938c300a76966ab8e9ac45e4185acc1a8
EtienneKyburz <114604643+ekyburz@users.noreply.github.com>
2024-02-01 20:12:04 +0100

    pet project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'about', to: 'about#index'
   get '/' => 'maine#index'
+  get 'sign_up' => 'registrations#new'
+  post 'users' => 'registrations#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
